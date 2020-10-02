1b017b9a037bdd97eccda0556a4f946c780e6c39
Nikita <aleshnikov9@gmail.com>
2020-10-02 20:37:27 +0300

    change dirs and readme

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM tvoyamamalama/pylinux
+#FROM python:3.8-slim
+
+#RUN apt-get update && \
+#    apt-get install -y --no-install-recommends build-essential libffi-dev
+
+ADD requirements.txt /
+
+RUN pip install -r requirements.txt
+
+WORKDIR /srv
+
+ADD src /srv
+
+#EXPOSE 8000
--- src/TaskManager/settings.py
+++ src/TaskManager/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '!oxzp&iz-=g_%u()rfp%fi(1kllkq*o$#olitkh-x2dsg$xp7%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -85,7 +85,7 @@ DATABASES = {
         'NAME': 'TaskManager',
         'USER': 'postgres',
         'PASSWORD': '348275723',
-        'HOST': 'localhost',
+        'HOST': 'task_manager_db',
         'PORT': '5432',
     }
 }
